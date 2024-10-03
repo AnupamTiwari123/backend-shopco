823b3a5d30f26af8d765f61883bb33f1445b3506
Anupam Tiwari <anupamtiwari242000@gmail.com>
2024-10-03 21:26:55 +0530

    commmite123

--- api/server.js
+++ api/server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require("path");
-
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
@@ -20,10 +19,19 @@ const newArrivals = require('../routes/newArrivals.routes');
 const paymentRoutes = require('../routes/paymentRoutes.routes');
 
 const app = express();
-const allowedOrigins = process.env.FRONTEND_URL ||'http://localhost:5173'; 
+const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    'http://localhost:5173'
+];
 
 app.use(cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
 }));
 
@@ -32,10 +40,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(session({
-    secret: 'your_secret_key',
+    secret: process.env.JWT_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false }
+    cookie: { secure: process.env.NODE_ENV === 'production' }
 }));
 
 mongoose.connect(process.env.MONGO_URI, {})
@@ -51,14 +59,16 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/reviews', reviewRoutes);
 app.use('/api/newarrivals', newArrivals);
 app.use('/api/payment', paymentRoutes);
+
 app.get("/", (req, res) => {
-res.send("hello there")
-    });
-    app.use((req, res) => {
-        console.log(`Route not found: ${req.method} ${req.originalUrl}`);
-        res.status(404).send('404: Not Found');
-    });
-    
+    res.send("hello there");
+});
+
+app.use((req, res) => {
+    console.log(`Route not found: ${req.method} ${req.originalUrl}`);
+    res.status(404).send('404: Not Found');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
