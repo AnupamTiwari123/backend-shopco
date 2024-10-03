9ebfbc5d38a7b173ae2de28e9c3ec8078225db55
Anupam Tiwari <anupamtiwari242000@gmail.com>
2024-10-03 21:35:40 +0530

    commmite1234567

--- api/server.js
+++ api/server.js
@@ -19,6 +19,8 @@ const newArrivals = require('../routes/newArrivals.routes');
 const paymentRoutes = require('../routes/paymentRoutes.routes');
 
 const app = express();
+
+// CORS settings
 const allowedOrigins = [
     process.env.FRONTEND_URL,
     'http://localhost:5173'
@@ -39,16 +41,20 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Session settings
 app.use(session({
     secret: process.env.JWT_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: process.env.NODE_ENV === 'production' }
+    cookie: {
+        secure: process.env.NODE_ENV === 'production'  
+    }
 }));
 
 mongoose.connect(process.env.MONGO_URI, {})
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error(err));
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
@@ -68,6 +74,7 @@ app.use((req, res) => {
     console.log(`Route not found: ${req.method} ${req.originalUrl}`);
     res.status(404).send('404: Not Found');
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
